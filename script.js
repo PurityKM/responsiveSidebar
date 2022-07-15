c56eec1438b95e2c7a78afd2c7e3e7a83a57d584
PurityKM <irungu1210@gmail.com>
2022-07-15 16:23:03 +0300

    commit

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ searchBtn.addEventListener("click", () => {
     sidebar.classList.remove("close");
 });
 
-
 modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
 
@@ -22,3 +21,28 @@ modeSwitch.addEventListener("click", () => {
         modeText.innerText = "Dark Mode"
     }
 });
+
+// const body = document.querySelector("body"),
+//     sidebar = body.querySelector(".sidebar"),
+//     toggle = body.querySelector(".toggle"),
+//     searchBtn = body.querySelector(".search-box"),
+//     modeSwitch = body.querySelector(".toggle-switch"),
+//     modeText = body.querySelector(".mode-text");
+
+// toggle.addEventListener("click", () => {
+//     sidebar.classList.toggle("close");
+// });
+// searchBtn.addEventListener("click", () => {
+//     sidebar.classList.remove("close");
+// });
+
+
+// modeSwitch.addEventListener("click", () => {
+//     body.classList.toggle("dark");
+
+//     if (body.classList.contains("dark")) {
+//         modeText.innerText = "Light Mode"
+//     } else {
+//         modeText.innerText = "Dark Mode"
+//     }
+// });
